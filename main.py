bb40928435ffe6b7521684f9a0dc30c1b56942c3
user <@>
2023-12-20 11:25:34 +0300

    test1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Names import names
+import random
 
 print(names)
 #im.mcko.ru/mo.php
@@ -6,4 +7,24 @@ print(names)
 d = dict()
 
 d["test1"] = 10
+#https://github.com/Boyarnikov/test_project_for_olimp
+#shorturl.at/nqDG1
 
+def generate_dataset(names_):
+    n = len(names_)
+    data = list()
+    project_ids = [i for i in range(n)]
+    random.shuffle(project_ids)
+    print(project_ids)
+
+    for i in range(n):
+        human = dict()
+        human["id"] = i
+        human["name"] = names_[i]
+        human["titleProject_id"] = project_ids[i]
+
+
+generate_dataset(names)
+
+#id, Name(в формате ФИО), titleProject_id(номер проекта, целое число), class(класс, в
+#формате цифра+буква), score(оценки, в формате целого числа или None).
